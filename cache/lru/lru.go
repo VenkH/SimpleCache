8fbc5ac2de311f15410ae7b302aaffa7707f854d
Venk <3494857305@qq.com>
2024-01-15 23:02:52 +0800

    feat: lru cache

---
+++ cache/lru/lru.go
@@ -0,0 +1,79 @@
+package lru
+
+import "container/list"
+
+// LruCache is an LRU cache. It is not safe for concurrent access.
+type LruCache struct {
+	maxBytes  int64
+	usedBytes int64
+	ll        *list.List
+	cache     map[string]*list.Element
+	// optional and executed when an entry is purged.
+	OnEvicted func(key string, value Value)
+}
+
+type entry struct {
+	key   string
+	value Value
+}
+
+// Value use Len to count how many bytes it takes
+type Value interface {
+	Len() int
+}
+
+// New is the Constructor of LruCache
+func New(maxBytes int64, onEvicted func(string, Value)) *LruCache {
+	return &LruCache{
+		maxBytes:  maxBytes,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: onEvicted,
+	}
+}
+
+// Get look ups a key's value
+func (c *LruCache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+	return
+}
+
+// RemoveOldest removes the oldest item
+func (c *LruCache) RemoveOldest() {
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
+		kv := ele.Value.(*entry)
+		delete(c.cache, kv.key)
+		c.usedBytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+// Add adds a value to the cache.
+func (c *LruCache) Add(key string, value Value) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		c.usedBytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		ele := c.ll.PushFront(&entry{key, value})
+		c.cache[key] = ele
+		c.usedBytes += int64(len(key)) + int64(value.Len())
+	}
+	for c.maxBytes != 0 && c.maxBytes < c.usedBytes {
+		c.RemoveOldest()
+	}
+}
+
+// Len the number of cache entries
+func (c *LruCache) Len() int {
+	return c.ll.Len()
+}
---
+++ cache/lru/lru_test.go
@@ -0,0 +1,56 @@
+package lru
+
+import (
+	"reflect"
+	"testing"
+)
+
+type String string
+
+func (d String) Len() int {
+	return len(d)
+}
+
+func TestGet(t *testing.T) {
+	lru := New(int64(0), nil)
+	lru.Add("key1", String("1234"))
+	if v, ok := lru.Get("key1"); !ok || string(v.(String)) != "1234" {
+		t.Fatalf("cache hit key1=1234 failed")
+	}
+	if _, ok := lru.Get("key2"); ok {
+		t.Fatalf("cache miss key2 failed")
+	}
+}
+
+func TestRemoveoldest(t *testing.T) {
+
+	k1, k2, k3 := "key1", "key2", "key3"
+	v1, v2, v3 := "value1", "value2", "value3"
+	cap := len(v1 + v2 + k1 + k2)
+	lruCache := New(int64(cap), nil)
+	lruCache.Add(k1, String(v1))
+	lruCache.Add(k2, String(v2))
+	lruCache.Add(k3, String(v3))
+
+	if _, ok := lruCache.Get(k1); ok || lruCache.Len() != 2 {
+		t.Fatalf("Removeoldest key1 failed")
+	}
+}
+
+func TestOnEvicted(t *testing.T) {
+	keys := make([]string, 0)
+	callback := func(key string, value Value) {
+		keys = append(keys, key)
+	}
+	lru := New(int64(10), callback)
+	lru.Add("key1", String("123456"))
+	lru.Add("k2", String("k2"))
+	lru.Add("k3", String("k3"))
+	lru.Add("k4", String("k4"))
+
+	expect := []string{"key1", "k2"}
+
+	if !reflect.DeepEqual(expect, keys) {
+		t.Fatalf("Call OnEvicted failed, expect keys equals to %s", expect)
+	}
+}
